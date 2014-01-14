f9a948b5d4d9c3c32ab4ec0cf8026c7712aaddf7
Stephen Baynham <stephen.baynham@gdsx.com>
2014-01-13 20:08:40 -0600

    DataPayload for new furnace complete.

---
+++ DataPayloader.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityAssetsLib;
+using UnityAssetsLib.FileTypes;
+using UnityAssetsLib.ObjTypes;
+
+namespace Furnace
+{
+    public class DataPayloader
+    {
+        private string mScriptAssemblyName;
+        private string mScriptNamespace;
+        private string mScriptClassName;
+
+        public DataPayloader(string assemblyName, string scriptNamespace, string scriptClassName)
+        {
+            mScriptAssemblyName = assemblyName;
+            mScriptNamespace = scriptNamespace;
+            mScriptClassName = scriptClassName;
+        }
+
+        public bool Inject(string injectionFile)
+        {
+            //Build assembly path to use
+            string assemblyPath = "";
+            if (Path.IsPathRooted(injectionFile))
+                assemblyPath = Path.Combine(Path.GetDirectoryName(injectionFile), mScriptAssemblyName);
+            else
+                assemblyPath = Path.Combine(Path.Combine("..", Path.GetDirectoryName(injectionFile)), mScriptAssemblyName);
+
+            //Try to open & parse file
+            if (!File.Exists(injectionFile))
+            {
+                Console.WriteLine("Could not locate mainData file.");
+                return false;
+            }
+
+            SwappableEndianBinaryReader reader = new SwappableEndianBinaryReader(File.OpenRead(injectionFile));
+            AssetsFile file = new AssetsFile();
+            file.Read(reader);
+            reader.Close();
+
+            //Remove injected leather/neolith monobehaviours & monoscripts
+            List<uint> scriptLocalIndexes = new List<uint>();
+            RemoveObjects<MonoScript>(file, obj => {
+                    if (obj.Assembly.IndexOf("Leather", StringComparison.OrdinalIgnoreCase) >= 0 || obj.Assembly.IndexOf("Neolith", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        scriptLocalIndexes.Add(obj.Info.Index);
+                        return true;
+                    } else
+                        return false;
+            });
+
+            RemoveObjects<MonoBehaviour>(file, obj => {
+                return obj.ScriptFileIndex == 0 && scriptLocalIndexes.Contains(obj.ScriptLocalIndex);
+            });
+
+            WipeAndReAddToMonoManager(file, assemblyPath);
+
+            MonoScript newScript = new MonoScript(mScriptClassName, 0, 0, mScriptClassName, mScriptNamespace, assemblyPath, 0);
+            file.Objects.Add(newScript);
+
+            file.Header.ReIndex(file.Objects);
+
+            GameObject injectionObj = GetInjectionObj(file);
+            file.Objects.Add(new MonoBehaviour(0, injectionObj.Info.Index, 0, newScript.Info.Index, ""));
+
+            SwappableEndianBinaryWriter writer = new SwappableEndianBinaryWriter(File.Open(injectionFile, FileMode.Create));
+            file.Write(writer);
+            writer.Close();
+
+            return true;
+        }
+
+        private GameObject GetInjectionObj(AssetsFile file)
+        {
+            foreach (GameObject obj in file.Objects.OfType<GameObject>())
+            {
+                if (string.Equals(obj.Name, "UI Root", StringComparison.OrdinalIgnoreCase) || string.Equals(obj.Name, "RustServer", StringComparison.OrdinalIgnoreCase))
+                    return obj;
+            }
+
+            return null;
+        }
+
+        private void RemoveObjects<T>(AssetsFile file, Func<T, bool> objRemovalTest) where T : UnityType
+        {
+            for (int i = file.Objects.Count - 1; i >= 0; i--)
+            {
+                if (file.Objects[i] is T && objRemovalTest((T)file.Objects[i]))
+                    file.Objects.RemoveAt(i);
+            }
+        }
+
+        private void WipeAndReAddToMonoManager(AssetsFile file, string assemblyPath)
+        {
+            foreach (UnityType obj in file.Objects)
+            {
+                if (obj is MonoManager)
+                {
+                    MonoManager manager = (MonoManager)obj;
+
+                    for (int assIndex = manager.Assemblies.Count - 1; assIndex >= 0; assIndex--)
+                    {
+                        if (manager.Assemblies[assIndex].IndexOf("Leather", StringComparison.OrdinalIgnoreCase) >= 0 || manager.Assemblies[assIndex].IndexOf("Neolith", StringComparison.OrdinalIgnoreCase) >= 0)
+                        {
+                            manager.Assemblies.RemoveAt(assIndex);
+                        }
+                    }
+
+                    manager.Assemblies.Add(assemblyPath);
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,76 +14,23 @@ namespace Furnace
     {
         static void Main(string[] args)
         {
-            DoIt();
-            Console.WriteLine("Press any key to end...");
-            Console.ReadKey();
-        }
-
-        static void DoIt()
-        {
-            if (!File.Exists("mainData"))
+            bool successfulPayloadInjection = false;
+            try
             {
-                Console.WriteLine("Could not locate mainData file.");
+                DataPayloader payloader = new DataPayloader(@"LeatherLoader", "LeatherLoader", "ModBootstrapper");
+                successfulPayloadInjection = payloader.Inject("mainData");
+            } catch (Exception e)
+            {
+                Console.WriteLine("Error while injecting payload: " + e.ToString());
+            }
+
+            if (!successfulPayloadInjection)
+            {
+                Console.WriteLine("Couldn't inject payload to assets file.");
                 return;
             }
-            SwappableEndianBinaryReader reader = new SwappableEndianBinaryReader(File.OpenRead("mainData"));
-            AssetsFile file = new AssetsFile();
-            file.Read(reader);
-            reader.Close();
-
-            List<MonoScript> scriptsToRemove = new List<MonoScript>();
-            foreach (UnityType obj in file.Objects)
-            {
-                if (obj is MonoManager)
-                {
-                    MonoManager manager = (MonoManager)obj;
-
-                    for (int assIndex = manager.Assemblies.Count - 1; assIndex >= 0; assIndex--)
-                    {
-                        if (manager.Assemblies[assIndex].IndexOf("Leather", StringComparison.OrdinalIgnoreCase) >= 0 || manager.Assemblies[assIndex].IndexOf("Neolith", StringComparison.OrdinalIgnoreCase) >= 0)
-                        {
-                            manager.Assemblies.RemoveAt(assIndex);
-                        }
-                    }
-
-                    manager.Assemblies.Add("NeolithLoader.dll");
-                }
-                else if (obj is MonoScript)
-                {
-                    MonoScript script = (MonoScript)obj;
-
-                    if (script.Assembly.IndexOf("Leather", StringComparison.OrdinalIgnoreCase) >= 0 || script.Assembly.IndexOf("Neolith", StringComparison.OrdinalIgnoreCase) >= 0)
-                        scriptsToRemove.Add(script);
-                }
-            }
-
-            List<uint> scriptLocalIndexes = new List<uint>();
-            foreach (MonoScript script in scriptsToRemove)
-            {
-                scriptLocalIndexes.Add(script.Info.Index);
-                file.Objects.Remove(script);
-            }
-
-            List<MonoBehaviour> behavioursToRemove = new List<MonoBehaviour>();
-            foreach (UnityType obj in file.Objects)
-            {
-                if (obj is MonoBehaviour)
-                {
-                    MonoBehaviour behaviour = (MonoBehaviour)obj;
-                    if (behaviour.ScriptFileIndex == 0 && scriptLocalIndexes.Contains(behaviour.ScriptLocalIndex))
-                        behavioursToRemove.Add(behaviour);
-                }
-            }
-
-            foreach (MonoBehaviour behaviour in behavioursToRemove)
-                file.Objects.Remove(behaviour);
-
-            // TODO
-            //file.ReIndex();
-
-            SwappableEndianBinaryWriter writer = new SwappableEndianBinaryWriter(File.Open("mainData-2", FileMode.Create));
-            file.Write(writer);
-            writer.Close();
+            else
+                Console.WriteLine("Assets file data injection successful.");
         }
     }
 }
